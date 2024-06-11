31ba05e21ee90144c31904c9d571623963d4bddf
Ahmad <ahmad.new.m.v@gmail.com>
2024-06-11 09:43:53 +0200

    New features

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,50 +1,80 @@
 #!/usr/bin/python3
-"""States blueprint"""
+"""Amenity blueprint"""
 from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 
 
 @app_views.route("/amenities", strict_slashes=False)
-def all_amenitites():
-    """Reterive all cities from storage"""
+def all_amenities():
+    """Reterive all amenities from storage"""
     from models import storage
     from models.amenity import Amenity
 
-    states_dict = storage.all(Amenity)
-    states_list = []
-    for state in states_dict.values():
-        states_list.append(state.to_dict())
-    return jsonify(states_list)
+    amenities_dict = storage.all(Amenity)
+    amenities_list = []
+    for amenity in amenities_dict.values():
+        amenities_list.append(amenity.to_dict())
+    return jsonify(amenities_list)
 
-@app_views.route("/amenities/<uuid:city_id>", strict_slashes=False)
-def one_amenity(city_id):
-    """Reterive one city from storage"""
+
+@app_views.route("/amenities/<uuid:amenity_id>", strict_slashes=False)
+def one_amenity(amenity_id):
+    """Reterive one amenity from storage"""
     from models import storage
     from models.amenity import Amenity
 
-    one_state = storage.get(Amenity, str(city_id))
-    if one_state:
-        return jsonify(one_state.to_dict())
-    abort(404)
-
-@app_views.route("/amenities/<uuid:city_id>", strict_slashes=False, methods=['DELETE'])
-def delete_amenity(city_id):
-    """Delete city from the storage"""
-    from models import storage
-    from models.amenity import Amenity
-
-    state = storage.get(Amenity, str(city_id))
-    if not state:
+    amenity = storage.get(Amenity, str(amenity_id))
+    if not amenity:
         abort(404)
-    storage.delete(state)
+    return jsonify(amenity.to_dict())
+
+
+@app_views.route("/amenities/<uuid:amenity_id>", strict_slashes=False,
+                 methods=['DELETE'])
+def delete_amenity(amenity_id):
+    """Delete one amenity from storage"""
+    from models import storage
+    from models.amenity import Amenity
+
+    amenity = storage.get(Amenity, str(amenity_id))
+    if not amenity:
+        abort(404)
+    storage.delete(amenity)
     storage.save()
     return jsonify({})
 
-#@app_views.route("/states", strict_slashes=False, methods=['POST'])
-#def add_city():
-#    """Add new state in the storage"""
-#    from models import storage
-#    from models.amenity import State
 
-#    if not requsest.json:
-#        make_response(jsonify())
+@app_views.route("/amenities", strict_slashes=False, methods=['POST'])
+def add_amenity():
+    """Add new amentiy to storage"""
+    from models.amenity import Amenity
+
+    if not request.is_json:
+        abort(400, "Not a JSON")
+    request_data = request.get_json()
+    if "name" not in request_data:
+        abort(400, "Missing name")
+    new_amenity = Amenity(**request_data)
+    new_amenity.save()
+    return make_response(jsonify(new_amenity.to_dict()), 201)
+
+
+@app_views.route("/amenities/<uuid:amenity_id>", strict_slashes=False,
+                 methods=['PUT'])
+def update_amenity(amenity_id):
+    """Update amentiy info"""
+    from models import storage
+    from models.amenity import Amenity
+
+    amenity = storage.get(Amenity, str(amenity_id))
+    if not amenity:
+        abort(404)
+    if not request.is_json:
+        abort(400, "Not a JSON")
+    request_data = request.get_json()
+    for k, v in request_data.items():
+        if k == "id" or k == "updated_at" or k == "created_at":
+            continue
+        amenity.__dict__[k] = v
+    amenity.save()
+    return jsonify(amenity.to_dict())
