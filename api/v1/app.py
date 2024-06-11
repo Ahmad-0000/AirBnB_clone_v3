60b8a93bb0241574406f4bb90213708535d9144d
Ahmad <ahmad.new.m.v@gmail.com>
2024-06-11 21:51:42 +0200

    Allowed CORS for /* in 0.0.0.0

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,12 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 from models import storage
 from os import getenv
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
+CORS(app)
 
 
 @app.teardown_appcontext
